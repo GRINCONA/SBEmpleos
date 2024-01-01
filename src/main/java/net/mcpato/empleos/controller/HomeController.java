df4796c5ee00d8d508fd7b98b1ac1ba9d2164ad2
GRINCONA <guiller.seven@gmail.com>
2024-01-01 19:12:59 +0100

    second commit

---
+++ src/main/java/net/mcpato/empleos/controller/HomeController.java
@@ -0,0 +1,52 @@
+package net.mcpato.empleos.controller;
+
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+
+
+@Controller
+public class HomeController {
+    
+    @GetMapping("/")
+    public String mostrarHome(Model model){
+        /*
+        model.addAttribute("mensaje", "AAAAAAAAAAAAAA");
+        model.addAttribute("fecha", new Date()  );
+         */
+
+        String nombre = "Aux Contabilidad";
+        Date fechaPub = new Date();
+        double salario = 9000.0;
+        boolean vigente = true;
+
+        model.addAttribute("nombre", nombre);
+        model.addAttribute("fechaPub", fechaPub);
+        model.addAttribute("salario", salario);
+        model.addAttribute("vigente", vigente);
+       
+        return "home";
+    }
+
+    @GetMapping("/listado")
+    public String mostrarListado(Model model) {
+
+        List<String> lista = new LinkedList<String>();
+        lista.add("Ingeniero de Sistemas");
+        lista.add("Aux Contabilidad");
+        lista.add("Vendedor");
+        lista.add("Arquitecto");
+
+        model.addAttribute("empleos", lista);
+
+        return "listado";
+    }
+    
+}
